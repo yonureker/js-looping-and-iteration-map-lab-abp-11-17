02af7ec52dc2338a32044f0505b6d660a4ccf7ab
Onur Eker <yonureker@gmail.com>
2017-11-20 08:59:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ function lowerCaseDrivers(drivers){
 
 function nameToAttributes(drivers) {
    return drivers.map(driver => ({ firstName: driver.split(' ')[0], lastName: driver.split(' ')[1] }));
- }
+}
+
+ function attributesToPhrase(drivers) {
+    return drivers.map(driver => `${driver.name} is from ${driver.hometown}`);
+}
