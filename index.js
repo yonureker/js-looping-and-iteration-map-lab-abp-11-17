03811857687bf28a26b8cf441f81001f5a53271b
Onur Eker <yonureker@gmail.com>
2017-11-20 08:40:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Code your solution in this file.
-function lowerCaseDrivers (array) {
-  return array.map(function(x){
-    return x.toLowerCase()
+function lowerCaseDrivers(array){
+  return array.map(function(e){
+    return e.toLowerCase()
   })
 }
 
-/*function nameToAttributes (array) {
-  return array.map(array =>
-  let fName = name.split(" ")[0];
-  let lName = name.split(" ")[1];
-  return {firstName: fName, lastname: lName}
-})
+//function nameToAttributes (array) {
+//  return array.map(array =>
+//  let fName = name.split(" ")[0];
+//  let lName = name.split(" ")[1];
+//  return {firstName: fName, lastname: lName}
+//})
