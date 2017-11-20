b2488bf1036ea6eceb0626fb81de1f298296e7cc
Onur Eker <yonureker@gmail.com>
2017-11-20 08:52:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function lowerCaseDrivers(array){
 
 function nametoAttributes(names){
   names.map(function(name){
-    let fName = name.split(" ")[0]
-    let lName = name.split(" ")[1]
-    return {firstName: fName, lastName: lName}
+    let fName = name.split(" ")[0];
+    let lName = name.split(" ")[1];
+    return {firstName: fName, lastName: lName};
 })
 }
