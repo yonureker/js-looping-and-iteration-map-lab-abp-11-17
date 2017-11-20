f23477b2157447338c84c55b93074edff5ea4173
Onur Eker <yonureker@gmail.com>
2017-11-20 08:29:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file.
+function lowerCaseDrivers (array) {
+  return array.map(array => array.toLowerCase())
+}
+
+function nameToAttributes (array) {
+  return array.map(array => Object.assign({},array))
+}
